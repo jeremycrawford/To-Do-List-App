6273cfcdabc58b4490284c93e51dadfa9b4ea355
Jeremy Crawford <jcrawford.design@gmail.com>
2014-02-11 01:19:11 -0500

    Initial Commit

---
+++ test/spec/test.js
@@ -0,0 +1,25 @@
+/* global describe, it */
+
+(function () {
+    'use strict';
+
+    describe('In our Todo App', function () {
+        describe('when the add button is clicked', function () {
+            it('should add a new todo div when the input it populated', function () {
+
+              // Add text to the .new-todo input
+              $('.js-new-todo-input').val('Drink beer');
+
+              // Click the "add" button
+              $('.js-add-todo').click();
+
+              // Store the value of the first todo div's text
+              var firstTodoText = $('.todo-item').first().children('.description').text();
+
+
+              expect(firstTodoText).to.contain('Drink beer');
+
+            });
+        });
+    });
+})();
