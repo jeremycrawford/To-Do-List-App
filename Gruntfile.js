e54122e8908453f1c6dbd0e3dec976136862e8d2
Jeremy Crawford <jcrawford.design@gmail.com>
2014-02-12 01:12:41 -0500

    Modified gruntfile.js to fix glyphicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function (grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
+    console.log('here!', this.includeCompass);
     // Define the configuration for all the tasks
     grunt.initConfig({
 
